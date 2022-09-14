ed30cb691dbdb4964313328419a39482ddfbe2cb
tecsupaponte <112447494+tecsupaponte@users.noreply.github.com>
2022-09-14 11:05:55 -0500

    doneso

--- Ejercicio02/Ejercicio02/App.xaml.cs
+++ Ejercicio02/Ejercicio02/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Ejercicio02
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- Ejercicio02/Ejercicio02/MainPage.xaml.cs
+++ Ejercicio02/Ejercicio02/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,29 @@ namespace Ejercicio02
         public MainPage()
         {
             InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Page1());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TabbedPage1());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CarouselPage1());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ModalPage1());
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                var answer = await DisplayAlert("Question?",
+                    "Would you like to play a game?", "Yes", "No");
+                Debug.WriteLine($"Answer: {answer}");
+            };
         }
     }
 }
